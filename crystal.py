eac687c80db3e1feca20a28282f3a23fd6d4a10d
Tristochi <TristanLSzakacs@gmail.com>
2019-08-02 15:07:25 -0400

    Progress on crystal.py

--- crystal.py
+++ crystal.py
@@ -2,19 +2,41 @@ import clr
 clr.AddReference("CrystalDecisions.CrystalReports.Engine")
 from CrystalDecisions.CrystalReports.Engine import ReportDocument, Table
 clr.AddReference("CrystalDecisions.Shared")
-from CrystalDecisions.Shared import ExportFormatType, ExportOptions
+from CrystalDecisions.Shared import ExportFormatType, ExportOptions, ParameterField, ParameterFields, ParameterValues, ParameterDiscreteValue
+import datetime
 
+date_parameter = datetime.date.today() - datetime.timedelta(days=1)
 
 report = ReportDocument()
-report.Load('bal_gt_250g_aggregate.rpt')
+report.Load('I:\\Mikes_Crystals\CDC_ofac_daily.rpt')
 print('Locked and Loaded')
 
-report.SetDatabaseLogon('xp2', 'xp2user')
+param1 = ParameterField()
+param2 = ParameterField()
+val1 = ParameterValues()
+val2 = ParameterValues()
+date1 = ParameterDiscreteValue()
+date2 = ParameterDiscreteValue()
+
+
+param1.ParameterFieldName = BeginDate
+param2.ParameterFieldName = EndDate
+date1.Value = date_parameter
+date2.Value = date_parameter
+val1.Add(date1)
+val2.Add(date2)
+
+report.SetParameterValue("1", val1)
+report.SetParameterValue("2", val2)
+
+print('Parameters set.')
+
+report.SetDatabaseLogon('uwu', 'owo')
 print('Credentials set.')
 report.Refresh()
 print('Report refreshed.')
 
 exportOp = ExportOptions()
-report.ExportToDisk(ExportFormatType.RichText, "report.rtf")
+report.ExportToDisk(ExportFormatType.CharacterSeparatedValues, "report.csv")
 
 print('File Exported?')
